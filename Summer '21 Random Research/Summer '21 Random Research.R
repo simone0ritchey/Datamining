bb1010e62bd40496eaa62f7cea50b8d86875b92d
simoneritchey <simone.ritchey@gmail.com>
2021-07-29 15:51:03 -0500

    Just started random research file and also downloaded Landuse data

---
+++ Summer '21 Random Research/Summer '21 Random Research.R
@@ -0,0 +1,16 @@
+### Summer 21' Random Research
+### Simone Ritchey
+
+### Do First ###################################################################
+
+# Load Packages
+
+# Load Data
+
+################################################################################
+
+### PCA of landuse grouped by county, looking for which variables explain differences 
+### between counties the most.
+
+# Loading watershed land use data
+setwd("/Users/simone/Documents/UT/UrbanEco/Datamining/LandUse_RSQA_Data")
