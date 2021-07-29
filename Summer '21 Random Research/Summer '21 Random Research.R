ddc4d0cd4091a7ecc6e34a0848c603dd2d420db9
simoneritchey <simone.ritchey@gmail.com>
2021-07-29 16:01:50 -0500

    started filtering land use data to only include the PNSQA region

--- Summer '21 Random Research/Summer '21 Random Research.R
+++ Summer '21 Random Research/Summer '21 Random Research.R
@@ -1,16 +1,26 @@
-### Summer 21' Random Research
+### Which land use variables explain the most difference between PNSQA counties? 
 ### Simone Ritchey
 
 ### Do First ###################################################################
 
 # Load Packages
+library(ggbiplot)
+library(dplyr)
 
 # Load Data
+setwd("/Users/simone/Documents/UT/UrbanEco/Datamining/LandUse_RSQA_Data")
+watershed_data <- read.csv("RSQA_Characteristics_Data_WatershedData.csv")
 
 ################################################################################
 
-### PCA of landuse grouped by county, looking for which variables explain differences 
-### between counties the most.
+### PCA of land use grouped by county
 
 # Loading watershed land use data
 setwd("/Users/simone/Documents/UT/UrbanEco/Datamining/LandUse_RSQA_Data")
+watershed_data <- read.csv("RSQA_Characteristics_Data_WatershedData.csv")
+
+# Clean data for PCA
+
+  # Extract PNSQA land use
+  watershed_data <- filter()
+
